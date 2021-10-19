f335d82cb9501d5de5861c4764263f2c7a9db843
Theo Ferguson <ferguson.theo@gmail.com>
2021-10-19 09:50:47 -0400

    commit tree 952fdbfadedda34389561ed7540b44c5100e6f26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
+  def index
+    users = User.all
+    render json: users, status: :ok
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index] do
+    resources :items, only: [:show, :index, :create] 
+  end
+
+
 end
